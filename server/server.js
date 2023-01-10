94db49568e2050890bc2e49d0a6d1cf1ac05ac71
fsalas21 <49110010+fsalas21@users.noreply.github.com>
2023-01-10 20:46:41 -0300

    Connected to DB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 
 const app = express();
+const port = 5005;
 
-app.listen(5005);
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb://localhost/memoria_v1')
+    .then(() => console.log('Database connected'));
+
+app.get('/inicio', (req, res) => res.json({
+    message : 'Hola inicio',
+    date : new Date(),
+    campus : 'USM Casa Central'
+}));
+// app.get('/seguimiento');
+// app.get('/modificar_encuesta');
+// app.get('/encuesta');
+
+app.listen(port, () => console.log('Server up at port ' + port));
